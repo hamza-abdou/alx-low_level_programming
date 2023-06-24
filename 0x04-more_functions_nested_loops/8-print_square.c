25fb49b203e282f6792553ecabde5b11ef7ba831
hamza-abdou <abdouhamza423@gmail.com>
2023-06-23 17:57:03 -0700

    file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,17 +12,17 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		 _putchar('\n');
+		_putchar('\n');
 	}
-	else 
+	else
 	{
 		for (i = 0; i < size; i++)
-        	{
+		{
 			for (j = 0; j < size; j++)
 			{
 				_putchar(35);
 			}
-                _putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
