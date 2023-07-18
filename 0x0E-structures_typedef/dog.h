db71a0c04e7cc76d7d261e52c62b2a2ecb1f58ee
hamza-abdou <abdouhamza423@gmail.com>
2023-07-18 06:49:23 -0700

    file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *struct dog - declaration of the tog structure
+ *@name: pointeur
+ *@age: struct de type int
+ *@owner: struct pointeur de type char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
