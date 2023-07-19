b7a99bb24c37fc228c705b81ff686fb38fc02098
hamza-abdou <abdouhamza423@gmail.com>
2023-07-19 08:07:18 -0700

    file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *array_iterator - function given as a parameter on each element of an array
+ *@array: pointeus
+ *@size: variable
+ *@action: pointeurs
+ *Return: always 0 
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0;i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
